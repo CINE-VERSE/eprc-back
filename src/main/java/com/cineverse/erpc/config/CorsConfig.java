1b3ebc57916cfcde7a2538874a496674328bda31
yunjaeeun <yunjaeeun12@naver.com>
2024-06-04 09:58:16 +0900

    deploy: CI/CD 배포
    CORS 수정

--- src/main/java/com/cineverse/erpc/config/CorsConfig.java
+++ src/main/java/com/cineverse/erpc/config/CorsConfig.java
@@ -1,23 +1,26 @@
-//package com.cineverse.erpc.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins(
-//                        "http://localhost:5173"
-////                        "*"
-//                )
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//    }
-//}
+package com.cineverse.erpc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:8080", "http://api.pioms.shop"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setExposedHeaders(Arrays.asList("Authorization", "Set-Cookie", "Cookie"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
