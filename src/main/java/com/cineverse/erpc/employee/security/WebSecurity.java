373d2727f65da3920d6f549e1bb2bda422f1fc31
yunjaeeun <yunjaeeun12@naver.com>
2024-06-05 23:32:31 +0900

    test: nginx 테스트
    nginx 파일 업로드 문제 해결중

--- src/main/java/com/cineverse/erpc/employee/security/WebSecurity.java
+++ src/main/java/com/cineverse/erpc/employee/security/WebSecurity.java
@@ -46,38 +46,40 @@ public class WebSecurity {
         http.csrf((csrf) -> csrf.disable());  // 토큰으로 처리할 예정이기 때문에 필요가 없음
 
         http.cors().and().authorizeHttpRequests((auth) -> auth
-                .requestMatchers(new AntPathRequestMatcher("/health")).permitAll()  // /login 엔드포인트 허용
-                .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()  // /login 엔드포인트 허용
-                .requestMatchers(new AntPathRequestMatcher("/employees/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/notice_board/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/notice_comment/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/sales_opportunity/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/sales_opp_note/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/warehouse/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/product/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/target/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/account/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/account_note/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/contract/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/quotation/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/quotation/regist")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/quotation_note/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/order/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/order_note/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/collection/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/access/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/tax_invoice/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/sales/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/approval/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/delete/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/shipment/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/excel/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/issue/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
-                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**",
-                        "/api-docs", "/api-docs/**", "/v3/api-docs/**")
-                .permitAll()
-                .anyRequest().authenticated()  // 나머지 요청은 인증 필요
+//                .requestMatchers(new AntPathRequestMatcher("/health")).permitAll()  // /login 엔드포인트 허용
+//                .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()  // /login 엔드포인트 허용
+//                .requestMatchers(new AntPathRequestMatcher("/employees/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/notice_board/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/notice_comment/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/sales_opportunity/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/sales_opp_note/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/warehouse/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/product/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/target/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/account/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/account_note/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/contract/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/quotation/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/quotation/regist")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/quotation_note/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/order/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/order_note/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/collection/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/access/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/tax_invoice/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/sales/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/approval/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/delete/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/shipment/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/excel/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/issue/**")).permitAll()
+//                .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+//                .requestMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**",
+//                        "/api-docs", "/api-docs/**", "/v3/api-docs/**")
+//                .permitAll()
+//                .anyRequest().authenticated()  // 나머지 요청은 인증 필요
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().permitAll()
         ).authenticationManager(authenticationManager);
 
         http.addFilter(getAuthenticationFilter(authenticationManager));
